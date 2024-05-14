ea5a91311c5c9cd5a2b4bedadd5dfabd7bdf3734
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-05-14 16:23:29 -0500

    Update author.py

--- author.py
+++ author.py
@@ -3,8 +3,8 @@ class Author:
         self.__name = name
         self.__biography = biography
 
-    def get_name(self):
+    def get_name(self): # Returning the name of the author
         return self.__name
 
-    def get_biography(self):
+    def get_biography(self): # Returning the biography of the author
         return self.__biography
